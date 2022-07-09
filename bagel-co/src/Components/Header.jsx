aec15755e3b74c6f53942710c5a5eb3c03955a88
Becky Chen <chenb9@rpi.edu>
2022-07-09 14:33:14 -0400

    Update Header.jsx

--- bagel-co/src/Components/Header.jsx
+++ bagel-co/src/Components/Header.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
-
 
 function Header() {
   return (
     <HeaderContainer classname= 'HeaderContainer'>
-      HELLO
+      <CompanyName classname= 'CompanyName'>
+        BECKY'S BAGEL CO.
+      </CompanyName>
+      <LinkContainer classname='LinkContainer'>
+        <StyledLink to="">Home</StyledLink>
+        <StyledLink to="Menu">Menu</StyledLink>
+        <StyledLink to="Locations">Locations</StyledLink>
+        <StyledLink to="Contact">Contact</StyledLink>
+      </LinkContainer>
     </HeaderContainer>
   )
 
 }
 
 const HeaderContainer = styled.div`
-  height: 200px;
   background-color: #37515F;
+  height: 200px;
+  position: fixed;
+  width: 100%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+`
+
+const CompanyName = styled.div`
+  text-align: center;
+  font-size: 90px;
+  font-weight: bold;
+  transition: 0.2s;
+  width: 100%;
 `
 
 const LinkContainer = styled.div`
-  height: 100%;
+  width: 100%;
   display: flex;
+  justify-content: center;
 `
+
 const StyledLink = styled(Link) `
   all: unset;
   color: #fff;
@@ -30,7 +52,7 @@ const StyledLink = styled(Link) `
 
   transition: 200ms;
   &:hover {
-    color: #37515F;
+    font-weight: bold;
     cursor: pointer;
   }
 `
